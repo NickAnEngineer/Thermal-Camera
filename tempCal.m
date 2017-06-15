6cc109871d837f512294889d8c00b7051e8e644b
Nick Boone <naboone1@sheffield.ac.uk>
2017-06-15 09:19:05 +0100

    Fixed it
    Seem's there was nothing worng with the code, just my pc didn't want to encode MPEG4 video. Using the default Motion JPEG AVI works fine.

--- tempCal.m
+++ tempCal.m
@@ -10,15 +10,23 @@ function [ColourImage] = tempCal(fullFilename, in)
         case 1 % Raw intensities
 
         case 2 % Intensities fourth rooted
-			IMintensity = rawImage + 1 ;
-            convertedImage = (double(IMintensity)).^0.25 * in.contrastRange/(in.contrastRange).^0.25 ;
-            convertedImage = floor(IMintensity) ;
+			intensityImage = rawImage + 1 ;
+            convertedImage = (double(intensityImage)).^0.25 * in.contrastRange/(in.contrastRange).^0.25 ;
+            convertedImage = floor(intensityImage) ;
 			
 		case 3 % Calibrated Temperature Conversion
-			calData = load(in.tempMapLoc);
-			convertedImage = interp1(calData(:,2), calData(:,1), double(rawImage), 'nearest');
-			if in.units == 'c'
-				convertedImage = convertedImage - 273.15;
+			load(in.tempMapLoc);
+			if exists('calData', 'var')
+				intensityImage = rawImage;
+				% clip data to low end of cal
+				intensityImage(rawImage < calData(1,2)) = calData(1,2);
+				% clip data to high end of cal
+				intensityImage(rawImage > calData(end,2)) = calData(end,2);
+				
+				convertedImage = interp1(calData(:,2), calData(:,1), double(intensityImage), 'nearest');
+				if in.units == 'c'
+					convertedImage = convertedImage - 273.15;
+				end
 			end
     end
     
